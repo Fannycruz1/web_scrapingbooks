2276e5b7e959c8283c5e774cee2113557a7ee571
Fanny Cruz <fannycruz41@outlook.com>
2024-07-23 21:46:35 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,21 +18,31 @@ while(proceed):
     page = requests.get(url)
 
     soup = BeautifulSoup(page.text, "html.parser") 
-
+    # This accounts for the last page of the book list 
     if soup.title.text == "404 Not Found" :
         proceed = False
     else:
 
+        #use html tag that contails all of the books 
+        # the books are stores in a li list tag include the class link 
+        # use Beautiful soup library find_all function 
         all_books = soup.find_all("li", class_ = "col-xs-6 col-sm-4 col-md-3 col-lg-3")
-
+        #find_all returns a list element we can use a for loop to parse this data 
         for book in all_books :
             item = {}
-
+        
+            # create disctionary keys for the following information 
+            # title of the book , link of the product, and the price 
+            #.find finds the first image tag use .attrs it is extremely useful here in terms of finding the first occurance of the attribute 
             item['Title'] = book.find("img").attrs["alt"]
             item['Link'] = book.find("a").attrs["href"]
+            #use string parsing tool to extract all of the characters after the third 
+            #use .text to get the text part of the tag
             item['Price'] = book.find("p", class_ = "price_color").text[2:]
-
+            #for finding the status of the product use the strip tool to delete any unintended whitespace 
             item['Stock'] = book.find("p", class_="instock availability").text.strip()
+
+            #input the specific data correlated to the book into the master list of book attribute data titled data 
 
             data.append(item)
 
@@ -42,5 +52,6 @@ while(proceed):
     
     current_page +=1
 
+# Here we will use the panda library to insert the data into the excel sheet 
 df = pf.DataFrame(data)
 df.to_excel("books.xlsx")
